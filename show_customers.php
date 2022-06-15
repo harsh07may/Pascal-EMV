c105d34598fafbffd8836fb784a2154bb0991eb0
Harsh Naik <harsh07may@gmail.com>
2022-06-15 10:27:34 +0530

    fixed the goddamn table

--- show_customers.php
+++ show_customers.php
@@ -61,6 +61,28 @@
 		button {
 			margin: 10px;
 		}
+		#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #7a7a7a;
+  color: white;
+}
 	</style>
 </head>
 
@@ -99,7 +121,7 @@
 			<br>
 			<button type="button" name="customers" onclick="showCustomer()">Submit</button>
 			<br>
-			<table border="2">
+			<table border="2" id="customers">
 				<tr>
 					<th>NAME</th>
 					</th>
